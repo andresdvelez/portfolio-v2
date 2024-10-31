0e99b3b65d5e15fed1bfece42ef7586520825e71
Andres Velez <andres082403@gmail.com>
2024-10-30 21:59:21 -0500

    Fix height

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
         foreground: "var(--foreground)",
       },
       height: {
-        noheader: "calc(100dvh - 120px)",
+        noheader: "calc(100vh - 120px)",
       },
       fontFamily: {
         "saint-regus": "var(--font-saint-regus)",
